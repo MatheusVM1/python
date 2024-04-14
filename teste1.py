ad74f49784d1642308ee0829e84760e752ed0be3
MatheusVM1 <166969495+MatheusVM1@users.noreply.github.com>
2024-04-14 00:16:10 -0300

    Create teste1.py

---
+++ teste1.py
@@ -0,0 +1,6 @@
+bemestar = input("Oi Matheus do futuro, gostaria de saber se você está bem, se você está feliz e satisfeito com a sua vida, se você se tornou o que queria e está feito na vida. Você está bem? (sim/não): ")
+
+if bemestar == "sim":
+    print("Estou muito orgulhoso de você, ainda bem que você conseguiu tudo que poderia e realizou um dos meus sonhos.")
+else:
+    print("Que pena, poderia ter feito melhor. Infelizmente, ainda não conseguimos tudo.")
